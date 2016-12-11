470f715396e4fd5a2b01702e18dd317984d3b7df
Lorenzo Fermi <lorenzofermi@gmail.com>
2016-12-11 21:03:09 +0100

    Converted to REAL jQuery

--- js/script.js
+++ js/script.js
@@ -1,29 +1,31 @@
-document.addEventListener("DOMContentLoaded",
-  function (event) {
+$(document).ready(function(){
 
-    // Model
-    var users = [
-      { label: 'Lisa', gender: 'F', tmb: 'https://randomuser.me/api/portraits/women/37.jpg' },
-      { label: 'Lorenzo', gender: 'M', tmb: 'https://randomuser.me/api/portraits/men/83.jpg' },
-      { label: 'Silvia', gender: 'F', tmb: 'https://randomuser.me/api/portraits/women/57.jpg' },
-    ]  
+  // Model
+  var users = [
+    { label: 'Lisa', gender: 'F', tmb: 'https://randomuser.me/api/portraits/women/37.jpg' },
+    { label: 'Lorenzo', gender: 'M', tmb: 'https://randomuser.me/api/portraits/men/83.jpg' },
+    { label: 'Silvia', gender: 'F', tmb: 'https://randomuser.me/api/portraits/women/57.jpg' },
+  ]  
 
-    var list = document.getElementById("simpleList");
+  var list = $("#simpleList");
 
-    for (i in users){    
+  //for (var user of users){   
+  users.forEach(
+    function(user){
+      var label = user.label;    
+      var genderClass = user.gender == 'F' ? "female" : "male";
 
-      var row = document.createElement('li');
-      row.className = users[i].gender == 'F' ? "female" : "male"
+      var img_elem = $("<img></img>");
+      img_elem.addClass("tmb");
+      img_elem.attr("src", user.tmb);
 
-      var img_elem = document.createElement('img');
-      img_elem.src = users[i].tmb;
-      img_elem.className = 'tmb';
-      row.appendChild(img_elem);
+      var row = $("<li></li>")   
+                .addClass(genderClass)
+                .append(img_elem)
+                .append(label);
 
-      var label = users[i].label;
-      row.appendChild(document.createTextNode(label));
-
-      list.appendChild(row);
+      list.append(row);
     }
-  }
-);
+  );
+
+});
